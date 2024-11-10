f5319e431d897324d5701ed498cc87ff2358c1bf
YuwenZhou99 <144034975+YuwenZhou99@users.noreply.github.com>
2024-11-10 16:15:16 +0100

    Delete autoop/core/ml/model directory

--- autoop/core/ml/model/classification/__init__.py
+++
@@ -1,3 +0,0 @@
-from autoop.core.ml.model.classification.k_nearest_neighbours import KNN
-from autoop.core.ml.model.classification.random_forest import RandomForestRegressorModel
-from autoop.core.ml.model.classification.svr import SVR
--- autoop/core/ml/model/regression/__init__.py
+++
@@ -1,3 +0,0 @@
-from autoop.core.ml.model.regression.multiple_linear_regression import MultipleLinearRegression
-from autoop.core.ml.model.regression.lasso import Lasso
-from autoop.core.ml.model.regression.svm import SVM
